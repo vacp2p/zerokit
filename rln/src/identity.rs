15c331db36a02e4b8e0fa58c7481343946822a0e
Oskar Thoren <ot@oskarthoren.com>
2022-03-15 18:04:11 +0800

    Add identity poseidon test

--- rln/src/identity.rs
+++ rln/src/identity.rs
@@ -1,12 +1,12 @@
+// Adapted from
+// https://github.com/worldcoin/semaphore-rs/blob/main/src/identity.rs
+
 use num_bigint::{BigInt, Sign};
 use once_cell::sync::Lazy;
 use poseidon_rs::Poseidon;
 use sha2::{Digest, Sha256};
 
 use crate::util::{bigint_to_fr, fr_to_bigint};
-
-// Adapted from
-// https://github.com/worldcoin/semaphore-rs/blob/main/src/identity.rs
 
 static POSEIDON: Lazy<Poseidon> = Lazy::new(Poseidon::new);
 
@@ -60,5 +60,24 @@ impl Identity {
             .hash(vec![bigint_to_fr(&self.secret_hash())])
             .unwrap();
         fr_to_bigint(res)
+    }
+}
+
+#[cfg(test)]
+pub mod test {
+    use super::*;
+    use std::str::FromStr;
+
+    #[test]
+    fn test_identity() {
+        let id = Identity::new(b"message");
+        //println!("Commitment: {:#}", id.commitment());
+        // From https://github.com/appliedzkp/zk-kit/blob/1ea410456fc2b95877efa7c671bc390ffbfb5d36/packages/identity/tests/index.test.ts#L63-L70
+        let s = "1720349790382552497189398984241859233944354304766757200361065203741879866188";
+
+        let x = BigInt::from_str(s).unwrap();
+
+        assert!(id.commitment() == x);
+
     }
 }
