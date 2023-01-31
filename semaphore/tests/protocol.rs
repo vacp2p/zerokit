7d1af734a6a0dd0b340b9d2f008e248c98ab7127
Rahul Ghangas <rahulghangas0@gmail.com>
2023-01-31 20:52:48 +0530

    chore: cargo fmt

--- semaphore/tests/protocol.rs
+++ semaphore/tests/protocol.rs
@@ -2,11 +2,13 @@
 mod tests {
     use ark_bn254::Parameters;
     use ark_ec::bn::Bn;
-    use ark_groth16::{Proof as ArkProof};
-    use semaphore::{hash_to_field, identity::Identity, poseidon_tree::PoseidonTree, Field};
-    use semaphore_wrapper::protocol::{generate_nullifier_hash, generate_proof, generate_proof_rng, Proof, verify_proof};
+    use ark_groth16::Proof as ArkProof;
     use rand::{Rng, SeedableRng as _};
     use rand_chacha::ChaChaRng;
+    use semaphore::{hash_to_field, identity::Identity, poseidon_tree::PoseidonTree, Field};
+    use semaphore_wrapper::protocol::{
+        generate_nullifier_hash, generate_proof, generate_proof_rng, verify_proof, Proof,
+    };
     use serde_json::json;
 
     #[test]
@@ -71,7 +73,7 @@ mod tests {
             signal_hash,
             &mut rng,
         )
-            .unwrap()
+        .unwrap()
     }
 
     #[test]
@@ -89,26 +91,25 @@ mod tests {
         assert_eq!(
             json,
             json!([
-            [
-                "0x249ae469686987ee9368da60dd177a8c42891c02f5760e955e590c79d55cfab2",
-                "0xf22e25870f49388459d388afb24dcf6ec11bb2d4def1e2ec26d6e42f373aad8"
-            ],
-            [
                 [
-                    "0x17bd25dbd7436c30ea5b8a3a47aadf11ed646c4b25cc14a84ff8cbe0252ff1f8",
-                    "0x1c140668c56688367416534d57b4a14e5a825efdd5e121a6a2099f6dc4cd277b"
+                    "0x249ae469686987ee9368da60dd177a8c42891c02f5760e955e590c79d55cfab2",
+                    "0xf22e25870f49388459d388afb24dcf6ec11bb2d4def1e2ec26d6e42f373aad8"
                 ],
                 [
-                    "0x26a8524759d969ea0682a092cf7a551697d81962d6c998f543f81e52d83e05e1",
-                    "0x273eb3f796fd1807b9df9c6d769d983e3dabdc61677b75d48bb7691303b2c8dd"
+                    [
+                        "0x17bd25dbd7436c30ea5b8a3a47aadf11ed646c4b25cc14a84ff8cbe0252ff1f8",
+                        "0x1c140668c56688367416534d57b4a14e5a825efdd5e121a6a2099f6dc4cd277b"
+                    ],
+                    [
+                        "0x26a8524759d969ea0682a092cf7a551697d81962d6c998f543f81e52d83e05e1",
+                        "0x273eb3f796fd1807b9df9c6d769d983e3dabdc61677b75d48bb7691303b2c8dd"
+                    ]
+                ],
+                [
+                    "0x62715c53a0eb4c46dbb5f73f1fd7449b9c63d37c1ece65debc39b472065a90f",
+                    "0x114f7becc66f1cd7a8b01c89db8233622372fc0b6fc037c4313bca41e2377fd9"
                 ]
-            ],
-            [
-                "0x62715c53a0eb4c46dbb5f73f1fd7449b9c63d37c1ece65debc39b472065a90f",
-                "0x114f7becc66f1cd7a8b01c89db8233622372fc0b6fc037c4313bca41e2377fd9"
-            ]
-        ])
+            ])
         );
     }
-
 }
