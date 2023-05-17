c0a4bc21c7d9e57c753c490e9a95ddf306bbb61a
Rostyslav Tyshko <tyshko.rostyslav@gmail.com>
2023-05-17 11:04:23 +0200

    main

--- rln-cli/src/main.rs
+++ rln-cli/src/main.rs
@@ -31,3 +31,36 @@ enum Commands {
         config: PathBuf,
     },
 }
+
+fn main() -> Result<()>{
+    let cli = Cli::parse();
+
+    let mut state = State::default();
+
+    match &cli.command {
+        Some(Commands::New { tree_height, config }) => {
+            let resources = File::open(&config).expect("no file found");
+            state.rln = Some(RLN::new(*tree_height, resources)?);
+            Ok(())
+        }
+        Some(Commands::NewWithParams { tree_height, config }) => {
+            let mut resources: Vec<Vec<u8>> = Vec::new();
+            for filename in ["rln.wasm", "rln_final.zkey", "verification_key.json"] {
+                let fullpath = config.join(Path::new(filename));
+                let mut file = File::open(&fullpath)?;
+                let metadata = std::fs::metadata(&fullpath)?;
+                let mut buffer = vec![0; metadata.len() as usize];
+                file.read_exact(&mut buffer)?;
+                resources.push(buffer);
+            }
+            state.rln = Some(RLN::new_with_params(
+                *tree_height,
+                resources[0].clone(),
+                resources[1].clone(),
+                resources[2].clone(),
+            )?);
+            Ok(())
+        }
+        None => {Ok(())}
+    }
+}
