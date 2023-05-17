37647d3523968e024fa667eb18e644fe08a99033
Rostyslav Tyshko <tyshko.rostyslav@gmail.com>
2023-05-17 11:03:34 +0200

    cli

---
+++ rln-cli/src/main.rs
@@ -0,0 +1,12 @@
+use std::{path::{PathBuf, Path}, fs::File, io::Read};
+
+use clap::{Parser, Subcommand};
+use rln::public::RLN;
+use color_eyre::Result;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
