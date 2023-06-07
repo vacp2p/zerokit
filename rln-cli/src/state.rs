ecd056884cd45db2dcff2978a0e9bf5ef49457cf
tyshko-rostyslav <122977916+tyshko-rostyslav@users.noreply.github.com>
2023-06-07 13:18:01 +0200

    CLI state between calls  (#175)
    * add serialization
    
    * add config
    
    * change state
    
    * final touches

--- rln-cli/src/state.rs
+++ rln-cli/src/state.rs
@@ -1,6 +1,23 @@
+use color_eyre::Result;
 use rln::public::RLN;
+use std::fs::File;
+
+use crate::config::{InnerConfig, Config};
 
 #[derive(Default)]
 pub(crate) struct State<'a> {
     pub rln: Option<RLN<'a>>,
+}
+
+impl<'a> State<'a> {
+    pub(crate) fn load_state() -> Result<State<'a>> {
+        let config = Config::load_config()?;
+        let rln = if let Some(InnerConfig { file, tree_height }) = config.inner {
+            let resources = File::open(&file)?;
+            Some(RLN::new(tree_height, resources)?)
+        } else {
+            None
+        };
+        Ok(State { rln })
+    }
 }
