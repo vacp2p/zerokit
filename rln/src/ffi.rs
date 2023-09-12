c99e9311391075247db02cb6a36b2a61776f3319
rymnc <43716372+rymnc@users.noreply.github.com>
2023-09-12 14:08:15 +0530

    chore(rln): expose leaves_set api

--- rln/src/ffi.rs
+++ rln/src/ffi.rs
@@ -263,6 +263,12 @@ pub extern "C" fn get_leaf(ctx: *mut RLN, index: usize, output_buffer: *mut Buff
 
 #[allow(clippy::not_unsafe_ptr_arg_deref)]
 #[no_mangle]
+pub extern "C" fn leaves_set(ctx: *mut RLN) -> usize {
+    ctx.process().leaves_set()
+}
+
+#[allow(clippy::not_unsafe_ptr_arg_deref)]
+#[no_mangle]
 pub extern "C" fn set_next_leaf(ctx: *mut RLN, input_buffer: *const Buffer) -> bool {
     call!(ctx, set_next_leaf, input_buffer)
 }
