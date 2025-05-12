fc9e84b0fa0f7a01cf867a08b5b453fbf3b05819
Ben-PH <benph@vac.dev>
2025-05-12 15:36:22 +0200

    use deterministic+portable PRNG for tree nodes

--- utils/benches/imt_benchy.rs
+++ utils/benches/imt_benchy.rs
@@ -1,8 +1,13 @@
 use light_poseidon::{PoseidonBytesHasher, PoseidonHasher};
+use rand::RngCore;
+use rand_chacha::ChaCha8Rng;
+use rand_core::SeedableRng;
 use rln::{circuit::Fr, utils::fr_to_bytes_le};
 use zerokit_utils::Poseidon;
-use zk_kit_lean_imt::{hashed_tree::{HashedLeanIMT, LeanIMTHasher}, lean_imt::*};
-
+use zk_kit_lean_imt::{
+    hashed_tree::{HashedLeanIMT, LeanIMTHasher},
+    lean_imt::*,
+};
 
 const ROUND_PARAMS: [(usize, usize, usize, usize); 8] = [
     (2, 8, 56, 0),
@@ -14,7 +19,34 @@ const ROUND_PARAMS: [(usize, usize, usize, usize); 8] = [
     (8, 8, 64, 0),
     (9, 8, 63, 0),
 ];
+// ChaCha8Rng is chosen for its portable determinism
+struct HashMockStream {
+    rng: ChaCha8Rng,
+}
 
+impl HashMockStream {
+    fn seeded_stream(seed: u64) -> Self {
+        let rng = ChaCha8Rng::seed_from_u64(seed);
+        Self { rng }
+    }
+}
+
+impl Iterator for HashMockStream {
+    type Item = [u8; 32];
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut res = [0; 32];
+        self.rng.fill_bytes(&mut res);
+        Some(res)
+    }
+}
+lazy_static::lazy_static! {
+    static ref LEAVES: [[u8; 32]; 40] = HashMockStream::seeded_stream(42)
+        .take(40)
+        .collect::<Vec<_>>()
+        .try_into()
+        .unwrap();
+}
 
 struct BenchyIFTHasher;
 struct BenchyLightPosHasher;
@@ -22,7 +54,10 @@ struct BenchyLightPosHasher;
 impl<const N: usize> LeanIMTHasher<N> for BenchyIFTHasher {
     fn hash(input: &[u8]) -> [u8; N] {
         let hasher = Poseidon::<Fr>::from(&ROUND_PARAMS);
-        let input_as_frs: Vec<_> = input.chunks(N).map(|ch| rln::utils::bytes_le_to_fr(ch).0).collect();
+        let input_as_frs: Vec<_> = input
+            .chunks(N)
+            .map(|ch| rln::utils::bytes_le_to_fr(ch).0)
+            .collect();
         let res = hasher.hash(&input_as_frs).unwrap();
         let byte_vec: Vec<u8> = fr_to_bytes_le(&res);
         let mut res = [0; N];
@@ -37,15 +72,21 @@ impl<const N: usize> LeanIMTHasher<N> for BenchyIFTHasher {
 impl LeanIMTHasher<32> for BenchyLightPosHasher {
     fn hash(input: &[u8]) -> [u8; 32] {
         let mut hasher = light_poseidon::Poseidon::<Fr>::new_circom(1).unwrap();
-        let chunks: Vec< &[u8] > = input.chunks(32).collect();
+        let chunks: Vec<&[u8]> = input.chunks(32).collect();
         hasher.hash_bytes_le(&chunks).unwrap()
     }
 }
 fn benchy_prototype_code() {
     let mut tree = HashedLeanIMT::<32, BenchyIFTHasher>::new(&[], BenchyIFTHasher).unwrap();
-    tree.insert(&[1; 32]);
-    tree.insert(&[2; 32]);
-    tree.insert_many(&[[4; 32], [4; 32], [5; 32]]).unwrap();
+    let mut genned_hashes = LEAVES.iter();
+    tree.insert(genned_hashes.next().unwrap());
+    tree.insert(genned_hashes.next().unwrap());
+    tree.insert_many(&[
+        *genned_hashes.next().unwrap(),
+        *genned_hashes.next().unwrap(),
+        *genned_hashes.next().unwrap(),
+    ])
+    .unwrap();
 
     println!("Tree root: {:?}", tree.root().unwrap());
     println!("Tree depth: {}", tree.depth());
