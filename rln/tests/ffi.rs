73c29e17a971e31cc8664f4e1ea82568f36cc77f
sydhds <sylvain@status.im>
2025-05-30 12:09:22 +0200

    Fix unit test

--- rln/tests/ffi.rs
+++ rln/tests/ffi.rs
@@ -1433,7 +1433,7 @@ mod stateless_test {
         for _ in 0..number_of_inputs {
             inputs.push(Fr::rand(&mut rng));
         }
-        let inputs_ser = vec_fr_to_bytes_le(&inputs).unwrap();
+        let inputs_ser = vec_fr_to_bytes_le(&inputs);
         let input_buffer = &Buffer::from(inputs_ser.as_ref());
 
         let expected_hash = utils_poseidon_hash(inputs.as_ref());
