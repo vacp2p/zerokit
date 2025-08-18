31fad5477297d4d6802871f526f584c7f3c18b90
Vinh Trịnh <108657096+vinhtc27@users.noreply.github.com>
2025-08-19 01:51:13 +0700

    chore: update wrong merged file

--- rln-cli/src/state.rs
+++ rln-cli/src/state.rs
@@ -1,7 +1,7 @@
 use std::io::Cursor;
 
 use color_eyre::Result;
-use rln::{circuit::TEST_TREE_HEIGHT, public::RLN};
+use rln::{circuit::TEST_TREE_DEPTH, public::RLN};
 use serde_json::Value;
 
 use crate::config::Config;
@@ -16,11 +16,11 @@ impl State {
         let config = Config::load_config()?;
         let rln = if let Some(tree_config) = config.tree_config {
             let config_json: Value = serde_json::from_str(&tree_config)?;
-            let tree_height = config_json["tree_height"]
+            let tree_depth = config_json["tree_depth"]
                 .as_u64()
-                .unwrap_or(TEST_TREE_HEIGHT as u64);
+                .unwrap_or(TEST_TREE_DEPTH as u64);
             Some(RLN::new(
-                tree_height as usize,
+                tree_depth as usize,
                 Cursor::new(tree_config.as_bytes()),
             )?)
         } else {
