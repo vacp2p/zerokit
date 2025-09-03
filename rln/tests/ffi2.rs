cac44f52cda3276bd8ca772517bff192842b9312
sydhds <sylvain@status.im>
2025-09-03 11:47:18 +0200

    Add initial code for FFI verify_rln_proof

--- rln/tests/ffi2.rs
+++ rln/tests/ffi2.rs
@@ -1,4 +1,66 @@
 #[cfg(test)]
+#[cfg(not(feature = "stateless"))]
+mod test {
+    // std
+    use std::ops::Deref;
+    // third-party
+    use rand::Rng;
+    use safer_ffi::boxed::Box_;
+    // internal
+    use rln::circuit::{Fr, TEST_TREE_DEPTH};
+    use rln::ffi2::{ffi2_generate_rln_proof, ffi2_key_gen, ffi2_rln_try_new, ffi2_verify_rln_proof, CFr, FFI2_RLNWitnessInput};
+    use rln::hashers::{
+        hash_to_field_le,
+        poseidon_hash as utils_poseidon_hash,
+    };
+
+    const NO_OF_LEAVES: usize = 256;
+
+    #[test]
+    // Computes and verifies an RLN ZK proof using FFI APIs
+    fn ffi2_test_rln_proof_ffi() {
+
+        let user_message_limit = Fr::from(100);
+
+        let rln = ffi2_rln_try_new(TEST_TREE_DEPTH).unwrap();
+
+        // We generate a new identity pair
+        let key_gen = ffi2_key_gen();
+        let id_secret_hash = &key_gen[0];
+        let id_commitment = &key_gen[1];
+        let identity_index: usize = NO_OF_LEAVES;
+
+        // We generate a random signal
+        let mut rng = rand::thread_rng();
+        let signal: [u8; 32] = rng.gen();
+
+        // We generate a random epoch
+        let epoch = hash_to_field_le(b"test-epoch");
+        // We generate a random rln_identifier
+        let rln_identifier = hash_to_field_le(b"test-rln-identifier");
+        // We generate a external nullifier
+        let external_nullifier = utils_poseidon_hash(&[epoch, rln_identifier]);
+        // We choose a message_id satisfy 0 <= message_id < MESSAGE_LIMIT
+        let message_id = Fr::from(1);
+
+        let rate_commitment = utils_poseidon_hash(&[*id_commitment.deref(), user_message_limit]);
+
+        let witness_input = Box_::new(FFI2_RLNWitnessInput {
+            identity_secret: id_secret_hash.into(),
+            user_message_limit: CFr::from(user_message_limit).into(),
+            message_id: CFr::from(message_id).into(),
+            external_nullifier: CFr::from(external_nullifier).into(),
+            tree_index: identity_index as u64,
+            signal: signal.to_vec().into_boxed_slice().into(),
+        });
+        let rln_proof = ffi2_generate_rln_proof(&rln, witness_input).unwrap();
+
+        let success = ffi2_verify_rln_proof(&rln, rln_proof, signal.as_slice().into());
+        assert!(success);
+    }
+}
+
+#[cfg(test)]
 mod general_tests {
 
     // use ark_std::{rand::thread_rng, UniformRand};
