338c63df8800d16f795901c58e630e9f72e24e88
vinhtc27 <vinhtc27@gmail.com>
2025-10-27 15:51:04 +0700

    refactor: split ffi2 module into rln, tree and utils

---
+++ rln/src/ffi2/mod.rs
@@ -0,0 +1,10 @@
+#![allow(non_camel_case_types)]
+
+pub mod ffi_rln;
+pub mod ffi_tree;
+pub mod ffi_utils;
+
+#[cfg(feature = "headers")]
+pub fn generate_headers() -> ::std::io::Result<()> {
+    ::safer_ffi::headers::builder().to_file("rln.h")?.generate()
+}
