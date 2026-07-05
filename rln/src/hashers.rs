a781ce0a8da465617699bf2530ee53846eb49a7d
vinhtc27 <vinhtc27@gmail.com>
2026-07-05 19:12:22 +0700

    chore: TODO(backlog)

--- rln/src/hashers.rs
+++ rln/src/hashers.rs
@@ -9,9 +9,9 @@ use zerokit_utils::poseidon::{Poseidon, PoseidonError};
 
 use crate::circuit::{Fr, SecretFr};
 
+/// TODO(backlog): Generate these parameters
 /// These indexed constants hardcode the supported round parameters tuples (t, RF, RN, SKIP_MATRICES) for the Bn254 scalar field.
 /// SKIP_MATRICES is the index of the randomly generated secure MDS matrix.
-/// TODO: Generate these parameters
 const ROUND_PARAMS: [(usize, usize, usize, usize); 8] = [
     (2, 8, 56, 0),
     (3, 8, 57, 0),
