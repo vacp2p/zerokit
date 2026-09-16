417c9dc75eaaab684612e46c8dc5d932c9acc343
Dario Lipicar <lipigl@gmail.com>
2026-09-16 09:26:59 -0300

    Update nix/default.nix
    Co-authored-by: Vinh Trịnh <108657096+vinhtc27@users.noreply.github.com>

--- nix/default.nix
+++ nix/default.nix
@@ -86,7 +86,8 @@ in rustPlatform.buildRustPackage {
     ''}
 
     mkdir -p $out/include
-    cbindgen ./rln -l c > "$out/include/rln.h"
+    cargo run --manifest-path rln/Cargo.toml --features headers --bin generate_headers
+    cp -v rln.h "$out/include/rln.h"
 
     ${if windows-gnu then ''
     if [ ! -f "$out/bin/rln.dll" ] \
